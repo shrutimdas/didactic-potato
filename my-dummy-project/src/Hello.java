a0623989002e793a0a2cb102c5beb84711b837fa
Shruti <shruti.mdas@gmail.com>
2021-03-23 16:41:20 +0530

    trying while loop

--- my-dummy-project/src/Hello.java
+++ my-dummy-project/src/Hello.java
@@ -4,5 +4,12 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Hello git! from eclipse");
 		System.out.println("this is my first commited code");
+		
+		int i=0;
+		
+		while(i < 5) {
+			System.out.println(i);
+			i++;
+		}
 	}
 }
