613921e38425e8076bd204a7b5cf47a29dd8ce2f
Shruti <shruti.mdas@gmail.com>
2021-03-23 17:22:03 +0530

    Modifying file/code from repository 2

--- my-dummy-project/src/Hello.java
+++ my-dummy-project/src/Hello.java
@@ -2,14 +2,6 @@
 public class Hello {
 
 	public static void main(String args[]) {
-		System.out.println("Hello git! from eclipse");
-		System.out.println("this is my first commited code");
-		
-		int i=0;
-		
-		while(i < 5) {
-			System.out.println(i);
-			i++;
-		}
+		System.out.println("Hello this is repository 2 ");		
 	}
 }
